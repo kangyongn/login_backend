ceb98e6b68c664d2e8b567a980a6714a3ccec5f8
github username <j.yongnamkang@gmail.com>
2019-08-18 18:59:37 -0400

    finished user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,45 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :get_user, only: [:show, :update, :destroy]
+
+def index
+  @users = User.all
+  render json: @users
+end
+
+def show
+  render json: @user
+end
+
+def create
+  @user = User.new(user_params)
+  if @user.save
+    @token = JWT.encode({user_id: @user.id}, "secret")
+    render json: {user: @user, jwt: @token}, status: :created
+  else
+    render json: @user.errors, status: :unprocessable_entity
+  end
+end
+
+def update
+  if @user.update(user_params)
+    render json: @user
+  else
+    render json: @user.errors, status: :unprocessable_entity
+  end
+end
+
+def destroy
+  render json: @user
+  @user.destroy
+end
+
+private
+
+def user_params
+  params.permit(:email, :password, :password_confirmation)
+end
+
+def get_user
+  @user = User.find(params[:id])
+end
 end
