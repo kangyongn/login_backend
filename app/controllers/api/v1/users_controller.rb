da5eabb7ea1b447df8975cc65a58d183ac44acc4
github username <j.yongnamkang@gmail.com>
2019-08-18 19:02:27 -0400

    beautify users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,45 +1,45 @@
 class Api::V1::UsersController < ApplicationController
   before_action :get_user, only: [:show, :update, :destroy]
 
-def index
-  @users = User.all
-  render json: @users
-end
-
-def show
-  render json: @user
-end
-
-def create
-  @user = User.new(user_params)
-  if @user.save
-    @token = JWT.encode({user_id: @user.id}, "secret")
-    render json: {user: @user, jwt: @token}, status: :created
-  else
-    render json: @user.errors, status: :unprocessable_entity
+  def index
+    @users = User.all
+    render json: @users
   end
-end
 
-def update
-  if @user.update(user_params)
+  def show
     render json: @user
-  else
-    render json: @user.errors, status: :unprocessable_entity
   end
-end
 
-def destroy
-  render json: @user
-  @user.destroy
-end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @token = JWT.encode({user_id: @user.id}, "secret")
+      render json: {user: @user, jwt: @token}, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
 
-private
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
 
-def user_params
-  params.permit(:email, :password, :password_confirmation)
-end
+  def destroy
+    render json: @user
+    @user.destroy
+  end
 
-def get_user
-  @user = User.find(params[:id])
-end
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find(params[:id])
+  end
 end
